6a4d9d8277d093e24822d26d775c14b36df4c2d2
Raphael Lima <raphaellima365@gmail.com>
2024-12-01 20:47:52 -0300

    Estrutura inicial do projeto full stack

---
+++ backend/src/test/java/backend/lojavirtual/LojavirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojavirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
